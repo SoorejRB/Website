1ccac6610f5d92d032f8c2f138bd846e61471804
SoorejRB <soorejrb@gmail.com>
2021-09-27 20:17:05 +0530

    third commit

--- general/urls.py
+++ general/urls.py
@@ -1,4 +1,4 @@
-from .views import IndexView, ContactView, TermsAndConditions
+from .views import IndexView, ContactView, TermsAndConditions,CareerView
 from django.urls import path
 
 
@@ -6,6 +6,7 @@ app_name = "general"
 urlpatterns = [
     path("", IndexView.as_view(), name = "home"),
     path("contact/", ContactView.as_view(), name = "contact"),
+    path("career/", CareerView.as_view(), name = "career"),
     path("terms-and-conditions", TermsAndConditions.as_view(), name = "tandc"),
 
 ]
--- general/views.py
+++ general/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, FormView
 from .forms import ContactForm, CareerForm
 from django.urls import reverse_lazy
-
+from django.contrib import messages
 
 class IndexView(TemplateView):
     template_name = "general/index.html"
@@ -14,7 +14,19 @@ class ContactView(FormView):
     
     def form_valid(self, form):
         form.save()
+        messages.success(self.request,"Submitted Succesfully")
         return super().form_valid(form)
+
+class CareerView(FormView):
+    template_name = "general/career.html"
+    form_class = CareerForm
+    success_url = reverse_lazy("general:home")
+    
+    def form_valid(self, form):
+        form.save()
+        messages.success(self.request,"Submitted Succesfully")
+        return super().form_valid(form) 
+          
 
 class TermsAndConditions(TemplateView):
     template_name = "general/tandc.html"
