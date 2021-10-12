8016809da265ea2a0f45c1571ccb49de3bb3caf8
SoorejRB <soorejrb@gmail.com>
2021-10-12 18:55:39 +0530

    footer, t&c, requirements

--- general/urls.py
+++ general/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("", IndexView.as_view(), name = "home"),
     path("contact/", ContactView.as_view(), name = "contact"),
     path("career/", CareerView.as_view(), name = "career"),
-    path("terms-and-conditions", TermsAndConditions.as_view(), name = "tandc"),
+    path("terms-and-conditions/", TermsAndConditions.as_view(), name = "tandc"),
 
 ]
